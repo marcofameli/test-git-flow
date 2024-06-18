635b50b0ac7237c6926d370111f35f680ff03532
marquola <zordnew@gmail.com>
2024-06-18 00:11:39 -0300

    add sum to java code

--- test.java
+++ test.java
@@ -1,5 +1,12 @@
 public class test {
     public static void main(String[] args) {
         System.out.println("Hello World");
+        int a = 1;
+        int b = 2;
+        int soma = a + b;
+        System.out.println(soma);
+
+        
+        
     }
 }
